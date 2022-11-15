04d9b249cc25545407f30d5bd76fb12e977b7ba1
Pwshady <pwshady@gmail.com>
2022-11-15 19:54:53 +0300

    151122

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd00e6656601673f0d679aca6d16eee57d6d8028b',
+        'name' => 'fa2/fa2',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fa2/fa2' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd00e6656601673f0d679aca6d16eee57d6d8028b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
